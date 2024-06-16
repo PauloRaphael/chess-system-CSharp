e13dc2cce7f513ad18d7f826facf5e235b2050e3
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-06-16 16:09:35 -0300

    Tratando erros com exceção personalizada

--- Program.cs
+++ Program.cs
@@ -8,15 +8,23 @@ namespace XadrezConsole // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            try
+            {
+                Tabuleiro tab = new(8, 8);
 
-            Tela.ImprimitTabuleiro(tab);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Console.ReadLine();
+                Tela.ImprimitTabuleiro(tab);
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -20,15 +20,47 @@ namespace tabuleiro
             Pecas = new Peca[Linhas, Colunas];
         }
 
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
         public Peca Peca(int linha, int coluna)
         {
             return Pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos)
+        public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição invalida!");
+            }
         }
     }   
 }
