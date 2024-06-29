cb56eef175385e6b1ce0f8a6abd011a89dcf5853
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-06-28 21:15:35 -0300

    Jogada especial promocao

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,23 @@ namespace XadrezConsole.xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em cheque");
             }
+
+            Peca p = Tab.Peca(destino);
+
+
+            // # JogadaEspecial promocao
+            if (p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(jogadorAtual)))
             {
                 Xeque = true;
@@ -158,8 +175,6 @@ namespace XadrezConsole.xadrez
                 MudaJogador();
                 Turno++;
             }
-
-            Peca p = Tab.Peca(destino);
 
             // # JoagadaEspecial En Passant 
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
